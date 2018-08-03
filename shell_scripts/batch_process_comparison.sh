ff6849cb99478be3d1b07a3fe1c2907dd61df84f
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2018-08-03 17:33:26 -0400

    slurm_array

--- shell_scripts/batch_process_comparison.sh
+++ shell_scripts/batch_process_comparison.sh
@@ -8,25 +8,18 @@
 
 # for i in $(find ~/data/bbrener1/johnston_retina/raw_data/*.fastq -exec basename {} \;);
 # do
-# 	if [ ! -d ../quantification/hisat_stringtie/$i ]; then
 #
-# 		bash hisat_string_quant.sh $i ../quantification/comparison/hisat ~/data/bbrener1/johnston_retina/raw_data/;
-#     bash kallisto_quant.sh $i ../quantification/comparison/kallisto ~/data/bbrener1/johnston_retina/raw_data/;
-#     bash bowtie_stringtie_quant.sh $i ../quantification/comparison/bowtie ~/data/bbrener1/johnston_retina/raw_data/;
+#   bash hisat_string_quant.sh $i ../quantification/comparison/hisat ~/data/bbrener1/johnston_retina/raw_data/;
+#   bash kallisto_quant.sh $i ../quantification/comparison/kallisto ~/data/bbrener1/johnston_retina/raw_data/;
+#   bash bowtie_stringtie_quant.sh $i ../quantification/comparison/bowtie ~/data/bbrener1/johnston_retina/raw_data/;
 #
-# 	fi
 # done
 
-i=$(find ~/data/bbrener1/johnston_retina/raw_data/*.fastq -exec basename {} \;) | head -n $SLURM_ARRAY_TASK_ID | tail -n 1)
-do
-	if [ ! -d ../quantification/hisat_stringtie/$i ]; then
+i=$(find ~/data/bbrener1/johnston_retina/raw_data/*.fastq -exec basename {} \; | head -n $SLURM_ARRAY_TASK_ID | tail -n 1)
 
-    echo "Comparing"
-    echo "$i"
+bash hisat_string_quant.sh $i ../quantification/comparison/hisat ~/data/bbrener1/johnston_retina/raw_data/;
+bash kallisto_quant.sh $i ../quantification/comparison/kallisto ~/data/bbrener1/johnston_retina/raw_data/;
+bash bowtie_stringtie_quant.sh $i ../quantification/comparison/bowtie ~/data/bbrener1/johnston_retina/raw_data/;
 
-		bash hisat_string_quant.sh $i ../quantification/comparison/hisat ~/data/bbrener1/johnston_retina/raw_data/;
-    bash kallisto_quant.sh $i ../quantification/comparison/kallisto ~/data/bbrener1/johnston_retina/raw_data/;
-    bash bowtie_stringtie_quant.sh $i ../quantification/comparison/bowtie ~/data/bbrener1/johnston_retina/raw_data/;
-
-	fi
-done
+# 	if [ ! -d ../quantification/comparison/$i ]; then
+# 	fi
