b33c185f9e1ee42c36301c3e4af9249ddf01faea
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2018-08-03 16:08:06 -0400

    comparison

--- shell_scripts/bowtie_stringtie_quant.sh
+++ shell_scripts/bowtie_stringtie_quant.sh
@@ -8,10 +8,24 @@
 
 source activate ../johnston_retina
 
-bowtie2 -mp 9999,9999 -x human_index/hg38/genome -p 21 -U $3/$1 > $2/$1.sam
+bowtie2 -mp 9999,9999 -x ../human_aligner_index/human_index/hg38/genome -p 21 -U $3/$1 > $2/$1.strict.sam
+bowtie2 -mp -x ../human_aligner_index/human_index/hg38/genome -p 21 -U $3/$1 > $2/$1.sam
+samtools view -bS $2/$1.strict.sam > $2/$1.strict.bam
 samtools view -bS $2/$1.sam > $2/$1.bam
+samtools sort $2/$1.strict.bam -o $2/$1.strict.sorted.bam
 samtools sort $2/$1.bam -o $2/$1.sorted.bam
+stringtie -G gencode.v27.annotation.gtf -A $2/$1.strict.abundance.tsv $2/$1.strict.sorted.bam
 stringtie -G gencode.v27.annotation.gtf -A $2/$1.abundance.tsv $2/$1.sorted.bam
-bedtools genomecov -split -ibam $2/$1.sorted.bam -g human_index/hg38.chrom.sizes -bg > $2/$1.bedGraph
-LC_COLLATE=C sort -k1,1 -k2,2n $2/$1.bedGraph > $2/$1.sorted.bedGraph
-./bedGraphToBigWig $2/$1.sorted.bedGraph ./hg38.chrom.sizes $2/$1.bw
+
+echo "Strict" > cmp_align_penalty.txt
+cat $2/$1.strict.abundance.tsv | grep OPN1LW >> cmp_align_penalty.txt
+cat $2/$1.strict.abundance.tsv | grep OPN1MW >> cmp_align_penalty.txt
+echo "Permissive" >> cmp_align_penalty.txt
+cat $2/$1.abundance.tsv | grep OPN1LW >> cmp_align_penalty.txt
+cat $2/$1.abundance.tsv | grep OPN1MW >> cmp_align_penalty.txt
+
+# bedtools genomecov -split -ibam $2/$1.sorted.bam -g human_index/hg38.chrom.sizes -bg > $2/$1.bedGraph
+# LC_COLLATE=C sort -k1,1 -k2,2n $2/$1.bedGraph > $2/$1.sorted.bedGraph
+# ./bedGraphToBigWig $2/$1.sorted.bedGraph ./hg38.chrom.sizes $2/$1.bw
+
+# | grep -P "\@|NH:i:1" >
--- shell_scripts/hisat_string_quant.sh
+++ shell_scripts/hisat_string_quant.sh
@@ -8,8 +8,20 @@
 
 source activate ../johnston_retina
 
-hisat2 -q --dta -x human_index/hg38/genome -p 21 -U $3/$1 | grep -P "\@|NH:i:1" > $2/$1.sam
+hisat2 -q --dta -x human_index/hg38/genome -p 21 -U $3/$1 > $2/$1.sam
+cat $2/$1.sam | grep -P "\@|NH:i:1" > $2/$1.filtered.sam
 samtools view -bS $2/$1.sam > $2/$1.bam
+samtools view -bS $2/$1.filtered.sam > $2/$1.filtered.bam
 samtools sort $2/$1.bam -o $2/$1.sorted.bam
+samtools sort $2/$1.filtered.bam -o $2/$1.filtered.sorted.bam
 stringtie -G gencode.v27.annotation.gtf -A $2/$1.abundance.tsv $2/$1.sorted.bam
-bedtools genomecov -split -ibam $2/$1.sorted.bam -g human_index/hg38.chrom.sizes -bg > $2/$1.bedGraph
+stringtie -G gencode.v27.annotation.gtf -A $2/$1.filtered.abundance.tsv $2/$1.filtered.sorted.bam
+
+echo "Unfiltered:" > filter_compare.txt
+cat $2/$1.abundance.tsv | grep OPN1LW >> filter_compare.txt
+cat $2/$1.abundance.tsv | grep OPN1MW >> filter_compare.txt
+echo "Filtered:" >> filter_compare.txt
+cat $2/$1.filtered.abundance.tsv | grep OPN1LW >> filter_compare.txt
+cat $2/$1.filtered.abundance.tsv | grep OPN1MW >> filter_compare.txt
+
+# bedtools genomecov -split -ibam $2/$1.sorted.bam -g human_index/hg38.chrom.sizes -bg > $2/$1.bedGraph
