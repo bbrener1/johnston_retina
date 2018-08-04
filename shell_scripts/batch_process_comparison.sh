e524e8e467c6772f7762144a902bbdc54b1a4e2c
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2018-08-04 18:16:03 -0400

    bowtie_transcriptome

--- shell_scripts/batch_process_comparison.sh
+++ shell_scripts/batch_process_comparison.sh
@@ -19,7 +19,9 @@ i=$(find ~/data/bbrener1/johnston_retina/raw_data/*.fastq -printf '%f\n' | head 
 
 # bash hisat_string_quant.sh $i ../../quantification/comparison/hisat ~/data/bbrener1/johnston_retina/raw_data/;
 # bash kallisto_quant.sh $i ../../quantification/comparison/kallisto ~/data/bbrener1/johnston_retina/raw_data/;
-bash bowtie_stringtie_quant.sh $i ../../quantification/comparison/bowtie ~/data/bbrener1/johnston_retina/raw_data/;
+# bash bowtie_stringtie_quant.sh $i ../../quantification/comparison/bowtie ~/data/bbrener1/johnston_retina/raw_data/;
+bash bowtie_stringtie_quant.sh $i ../../quantification/comparison/bowtie_transcriptome ~/data/bbrener1/johnston_retina/raw_data/;
+
 
 # 	if [ ! -d ../quantification/comparison/$i ]; then
 # 	fi
--- shell_scripts/bowtie_stringtie_quant.sh
+++ shell_scripts/bowtie_stringtie_quant.sh
@@ -8,21 +8,21 @@
 
 source activate ../johnston_retina
 
-bowtie2 --mp 9999,9998 -x ../bowtie_index/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index -p 21 -U $3/$1 > $2/$1.strict.sam
-bowtie2 -x ../bowtie_index/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index -p 21 -U $3/$1 > $2/$1.sam
+bowtie2 --mp 9999,9998 -x ../gencode_transcriptome/gencode_bowtie_index -p 21 -U $3/$1 > $2/$1.strict.sam
+bowtie2 -x ../gencode_transcriptome/gencode_bowtie_index -p 21 -U $3/$1 > $2/$1.sam
 samtools view -bS $2/$1.strict.sam > $2/$1.strict.bam
 samtools view -bS $2/$1.sam > $2/$1.bam
 samtools sort $2/$1.strict.bam -o $2/$1.strict.sorted.bam
 samtools sort $2/$1.bam -o $2/$1.sorted.bam
-stringtie -G ../gencode_transcriptome/gencode.v27.annotation.gtf -A $2/$1.strict.abundance.tsv $2/$1.strict.sorted.bam > $2/$1.strict.gtf
-stringtie -G ../gencode_transcriptome/gencode.v27.annotation.gtf -A $2/$1.abundance.tsv $2/$1.sorted.bam > $2/$1.gtf
-
-echo "Strict" > $2/$1.cmp_align_penalty.txt
-cat $2/$1.strict.abundance.tsv | grep OPN1LW >> $2/$1.cmp_align_penalty.txt
-cat $2/$1.strict.abundance.tsv | grep OPN1MW >> $2/$1.cmp_align_penalty.txt
-echo "Permissive" >> $2/$1.cmp_align_penalty.txt
-cat $2/$1.abundance.tsv | grep OPN1LW >> $2/$1.cmp_align_penalty.txt
-cat $2/$1.abundance.tsv | grep OPN1MW >> $2/$1.cmp_align_penalty.txt
+# stringtie -G ../gencode_transcriptome/gencode.v27.annotation.gtf -A $2/$1.strict.abundance.tsv $2/$1.strict.sorted.bam > $2/$1.strict.gtf
+# stringtie -G ../gencode_transcriptome/gencode.v27.annotation.gtf -A $2/$1.abundance.tsv $2/$1.sorted.bam > $2/$1.gtf
+#
+# echo "Strict" > $2/$1.cmp_align_penalty.txt
+# cat $2/$1.strict.abundance.tsv | grep OPN1LW >> $2/$1.cmp_align_penalty.txt
+# cat $2/$1.strict.abundance.tsv | grep OPN1MW >> $2/$1.cmp_align_penalty.txt
+# echo "Permissive" >> $2/$1.cmp_align_penalty.txt
+# cat $2/$1.abundance.tsv | grep OPN1LW >> $2/$1.cmp_align_penalty.txt
+# cat $2/$1.abundance.tsv | grep OPN1MW >> $2/$1.cmp_align_penalty.txt
 
 # bedtools genomecov -split -ibam $2/$1.sorted.bam -g human_index/hg38.chrom.sizes -bg > $2/$1.bedGraph
 # LC_COLLATE=C sort -k1,1 -k2,2n $2/$1.bedGraph > $2/$1.sorted.bedGraph
