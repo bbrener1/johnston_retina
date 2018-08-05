c6456ad0c844cb362ae720996d770528407d65e7
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2018-08-05 03:48:51 -0400

    pileup

---
+++ shell_scripts/pileup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --mem-per-cpu=5G
+#SBATCH -t 500
+
+source activate ../johnston_retina
+
+samtools mpileup -l ../unique_loci -f ../gencode_transcriptome/gencode.v27.transcripts.fa $2/$1.sam > $2/$1.pile
