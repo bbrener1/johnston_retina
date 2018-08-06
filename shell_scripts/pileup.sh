f7076f51934774b34455cde27d34416fb329d6fd
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2018-08-06 14:11:25 -0400

    mw loci

--- shell_scripts/pileup.sh
+++ shell_scripts/pileup.sh
@@ -7,4 +7,5 @@
 
 source activate ../johnston_retina
 
-samtools mpileup -l ../unique_loci.txt -f ../hisat_index/GRCh38.primary_assembly $2/$1.sam > $2/$1.pile
+samtools mpileup -l ../unique_lw.txt -f ../hisat_index/GRCh38.primary_assembly $2/$1.sam > $2/$1.pile
+samtools mpileup -l ../unique_mw_ex5.txt -f ../hisat_index/GRCh38.primary_assembly $2/$1.sam >> $2/$1.pile
