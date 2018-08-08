b3984669e46b682a12ba1378eca9a4f9ed83535f
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2018-08-08 12:38:54 -0400

    consolidate

--- python_scripts/consolidate_pileups.py
+++ python_scripts/consolidate_pileups.py
@@ -6,5 +6,15 @@ samples = open(sys.argv[1]).readlines()
 days = open(sys.argv[2]).readlines()
 paths = open(sys.argv[3]).readlines()
 
+lw_positions = map(lambda x: x.split()[0],open(sys.argv[4]).readlines()))
+mw_positions = map(lambda x: x.split()[0],open(sys.argv[4]).readlines()))
+
+print lw_positions
+print mw_positions
+
+days = map(lambda x: int(x),days)
+set_days = set(days)
+
 for sample,day,path in zip(samples,days,paths):
     print str((sample,day,path))
+    sample_pile = open(path)
