84febc45c9f692bdb6730f6d93218b95a75a5bba
Boris M Brenerman <boris@Boriss-MacBook-Pro-2.local>
2018-08-09 01:51:35 -0400

    paired

--- shell_scripts/bowtie_stringtie_transcriptome_quant.sh
+++ shell_scripts/bowtie_stringtie_transcriptome_quant.sh
@@ -6,10 +6,12 @@
 #SBATCH -t 500
 #SBATCH -o slurm_job_out2.txt
 
-source activate ../johnston_retina
+#source activate ../johnston_retina
 
-bowtie2 --mp 9999,9998 -x ../gencode_transcriptome/gencode_bowtie_index -p 21 -U $3/$1 > $2/$1.strict.sam
-bowtie2 -x ../gencode_transcriptome/gencode_bowtie_index -p 21 -U $3/$1 > $2/$1.sam
+# bowtie2 --mp 9999,9998 -x ../gencode_transcriptome/gencode_bowtie_index -p 21 -U $3/$1 > $2/$1.strict.sam
+# bowtie2 -x ../gencode_transcriptome/gencode_bowtie_index -p 21 -U $3/$1 > $2/$1.sam
+bowtie2 --mp 9999,9998 -x ../gencode_transcriptome/gencode_bowtie_index -p 21 --interleaved $3/$1 > $2/$1.strict.sam
+bowtie2 -x ../gencode_transcriptome/gencode_bowtie_index -p 21 --interleaved $3/$1 > $2/$1.sam
 samtools view -bS $2/$1.strict.sam > $2/$1.strict.bam
 samtools view -bS $2/$1.sam > $2/$1.bam
 samtools sort $2/$1.strict.bam -o $2/$1.strict.sorted.bam
